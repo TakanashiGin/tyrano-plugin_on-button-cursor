d1f8c0e9ee761ad934b02a40a50cac650f252fa9
TakanashiGin <takanashi.gin.jul@gmail.com>
2021-02-04 07:59:26 +0900

    change storage param

--- onButtonCursor/init.js
+++ onButtonCursor/init.js
@@ -5,8 +5,12 @@ tyrano.plugin.kag.ftag.master_tag.set_onbutton_cursor = {
         storage: "default"
     },
     start: function(pm){
-        if (pm.content == "opt") this.kag.stat.on_button_cursor = pm.storage;
-        else if (pm.content == "run") $(".event-setting-element").css("cursor",this.kag.stat.on_button_cursor == "default"? "auto" : "url(./data/image/" + this.kag.stat.on_button_cursor + "),default");
+        if (pm.content == "opt") {
+            this.kag.stat.on_button_cursor = pm.storage;
+        } else if (pm.content == "run") {
+            var cursor = pm.storage || this.kag.stat.on_button_cursor;
+            $(".event-setting-element").css("cursor",cursor == "default"? "auto" : "url(./data/image/" + cursor + "),default");
+        }
         this.kag.ftag.nextOrder();
     }
 };
